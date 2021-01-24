679c650b805a14e113f53414b3d9631356040e01
Alexey <alexey-lopatenko@yandex.ru>
2021-01-24 19:18:34 +0300

    Создана первоначальная структура проекта

---
+++ app/src/main/java/android/example/sqlitekotlin/EditActivity.kt
@@ -0,0 +1,11 @@
+package android.example.sqlitekotlin
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.edit_activity)
+    }
+}
---
+++ app/src/main/java/android/example/sqlitekotlin/MainActivity.kt
@@ -0,0 +1,34 @@
+package android.example.sqlitekotlin
+
+import android.content.Intent
+import android.example.sqlitekotlin.db.MyDbManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.EditText
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    val myDbManager = MyDbManager(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myDbManager.openDb()
+    }
+
+    fun onClickNew(view: View){
+        val i = Intent(this, EditActivity::class.java)
+        startActivity(i)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
+    }
+}
