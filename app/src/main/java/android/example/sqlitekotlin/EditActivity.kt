9b47219715b7e109109f1620c6697b48844e30ca
Alexey <alexey-lopatenko@yandex.ru>
2021-01-31 17:50:41 +0300

    Доработал UI и сделал возможность добавления фото

--- app/src/main/java/android/example/sqlitekotlin/EditActivity.kt
+++ app/src/main/java/android/example/sqlitekotlin/EditActivity.kt
@@ -2,10 +2,25 @@ package android.example.sqlitekotlin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.solver.state.State
 
 class EditActivity : AppCompatActivity() {
+    var mainImageLayout = findViewById<View>(R.id.myImageLayout)
+    var fbAddImage = findViewById<View>(R.id.fbAddImage)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
+    }
+
+    fun onClickAddImage(view: View) {
+        mainImageLayout.visibility = View.VISIBLE
+        fbAddImage.visibility = View.GONE
+    }
+
+    fun onClickDeleteImage(view: View) {
+        mainImageLayout.visibility = View.GONE
+        fbAddImage.visibility = View.VISIBLE
     }
 }
