c7de58db104ff204bbe8330b407b902b21c1fac0
Alexey <alexey-lopatenko@yandex.ru>
2021-02-03 18:20:25 +0300

    Создал RecyclerView Adapter

--- app/src/main/java/android/example/sqlitekotlin/EditActivity.kt
+++ app/src/main/java/android/example/sqlitekotlin/EditActivity.kt
@@ -1,17 +1,48 @@
 package android.example.sqlitekotlin
 
+import android.app.Activity
+import android.content.Intent
+import android.example.sqlitekotlin.db.MyDbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import androidx.constraintlayout.solver.state.State
 
 class EditActivity : AppCompatActivity() {
-    var mainImageLayout = findViewById<View>(R.id.myImageLayout)
-    var fbAddImage = findViewById<View>(R.id.fbAddImage)
+    val myDbManager = MyDbManager(this)
+    val imageRequestCode = 10
+    var tempImageUri = "empty"
+
+    var mainImageLayout = findViewById<ImageView>(R.id.myImageLayout)
+    var fbAddImage = findViewById<ImageView>(R.id.fbAddImage)
+    var imMainImage = findViewById<ImageView>(R.id.imMainImage)
+    var edTitle = findViewById<EditText>(R.id.edTitle)
+    var edDesc = findViewById<EditText>(R.id.edDesc)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.edit_activity)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        myDbManager.openDb()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == Activity.RESULT_OK && requestCode == imageRequestCode) {
+            imMainImage.setImageURI(data?.data)
+            tempImageUri = data?.data.toString()
+        }
+
     }
 
     fun onClickAddImage(view: View) {
@@ -22,5 +53,19 @@ class EditActivity : AppCompatActivity() {
     fun onClickDeleteImage(view: View) {
         mainImageLayout.visibility = View.GONE
         fbAddImage.visibility = View.VISIBLE
+    }
+
+    fun onClickChooseImage(view: View) {
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, imageRequestCode)
+    }
+
+    fun onClickSave(view: View) {
+       val myTitle = edTitle.text.toString()
+       val myDesc = edDesc.text.toString()
+        if (myTitle != "" && myDesc != "") {
+           myDbManager.insertToDb(myTitle, myDesc, tempImageUri)
+        }
     }
 }
--- app/src/main/java/android/example/sqlitekotlin/MainActivity.kt
+++ app/src/main/java/android/example/sqlitekotlin/MainActivity.kt
@@ -1,25 +1,37 @@
 package android.example.sqlitekotlin
 
 import android.content.Intent
+import android.example.sqlitekotlin.db.MyAdapter
 import android.example.sqlitekotlin.db.MyDbManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.EditText
+import android.widget.LinearLayout
 import android.widget.TextView
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     val myDbManager = MyDbManager(this)
+    val myAdapter = MyAdapter(ArrayList())
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        myDbManager.closeDb()
     }
 
     override fun onResume() {
         super.onResume()
         myDbManager.openDb()
+        fillAdapter()
     }
 
     fun onClickNew(view: View){
@@ -27,8 +39,14 @@ class MainActivity : AppCompatActivity() {
         startActivity(i)
     }
 
-    override fun onDestroy() {
-        super.onDestroy()
-        myDbManager.closeDb()
+    fun init(){
+        val rcView = findViewById<RecyclerView>(R.id.rcView)
+        rcView.layoutManager = LinearLayoutManager(this)
+        rcView.adapter = myAdapter
     }
+
+    fun fillAdapter(){
+        myAdapter.updateAdapter(myDbManager.readDbData())
+    }
+
 }
